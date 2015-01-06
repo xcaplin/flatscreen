9e85e035f6c35702ac79f2b6435a0483fc4abb40
jason <jason.caplin@gmail.com>
2015-01-06 19:22:42 +0000

    heroku cheatsheet complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
 
-  root 'flats#show'
+#devise_for :users
+#  root 'flats#show'
+
+#  devise_scope :user do
+#    get 'logout' => 'devise/sessions#destroy', as:'logout'
+#  end
+
   get '/new' => 'flats#new'
-
-  devise_scope :user do
-    get 'logout' => 'devise/sessions#destroy', as:'logout'
-  end
-
   get '/index' => 'flats#index', as: 'index'
 
 
